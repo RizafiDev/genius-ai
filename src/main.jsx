b333686ba0f775d24162f283f9dcfb1680372678
RizafiDev <firmansyah.riza6@smk.belajar.id>
2024-09-25 00:23:59 +0700

    bismilah

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import Nav from './Nav.jsx'
-import Header from './Header.jsx'
-import Sponsor from './Sponsor.jsx'
-import Features from './Features.jsx'
-import Services from './services.jsx'
-import Pricing from './Pricing.jsx'
-import Tagline from './Tagline.jsx'
-import Footer from './Footer.jsx'
-import './index.css'
-import 'remixicon/fonts/remixicon.css'
-import AOS from 'aos';
-import 'aos/dist/aos.css'; // You can also use <link> for styles
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import Nav from "./Nav.jsx";
+import Header from "./Header.jsx";
+import Sponsor from "./Sponsor.jsx";
+import Features from "./Features.jsx";
+import Services from "./Services.jsx";
+import Pricing from "./Pricing.jsx";
+import Tagline from "./Tagline.jsx";
+import Footer from "./Footer.jsx";
+import "./index.css";
+import "remixicon/fonts/remixicon.css";
+import AOS from "aos";
+import "aos/dist/aos.css"; // You can also use <link> for styles
 // ..
 AOS.init();
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Nav />
     <Header />
@@ -25,5 +25,5 @@ createRoot(document.getElementById('root')).render(
     <Pricing />
     <Tagline />
     <Footer />
-  </StrictMode>,
-)
+  </StrictMode>
+);
